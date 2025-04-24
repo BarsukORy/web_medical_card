378e64b337fb3d2bf5a07e6cea826d0fcf7f1d73
Barsyk <georgiy.nikitin.2005@mail.ru>
2025-04-25 01:20:28 +0300

    деплой

--- back/back/settings.py
+++ back/back/settings.py
@@ -26,6 +26,11 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://85.192.31.82",
+    "http://85.192.31.82", 
+]
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
